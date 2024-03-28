c4d8179c41786d39826623005bc2891575fa456c
Tiago <ti.almeida58@gmail.com>
2024-03-28 08:31:28 -0300

    feat: moving functions to person_controller

--- src/controller/person_controller.rs
+++ src/controller/person_controller.rs
@@ -1,8 +1,17 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+use axum::extract::{Path, State};
+use axum::http::StatusCode;
+use axum::Json;
+use axum::response::IntoResponse;
 use serde::{Deserialize, Serialize};
 use time::{ Date };
+use tokio::sync::RwLock;
 use uuid::Uuid;
 
 time::serde::format_description!(date_format, Date, "[day]-[month]-[year]");
+
+pub type AppState = Arc<RwLock<HashMap<Uuid, Person>>>;
 
 #[derive(Clone, Serialize)]
 pub struct Person {
@@ -25,4 +34,43 @@ pub struct NewPerson {
     #[serde(rename = "nascimento", with = "date_format")]
     pub birth_date: Date,
     pub stack: Option<Vec<String>>
+}
+
+impl Person {
+    pub async fn find_person(
+        State(people): State<AppState>,
+        Path(person_id): Path<Uuid>
+    ) -> impl IntoResponse {
+        match people.read().await.get(&person_id) {
+            Some(person) => Ok(Json(person.clone())),
+            None => Err(StatusCode::NOT_FOUND)
+        }
+    }
+
+    pub async fn search_people(state: State<AppState>) -> impl IntoResponse {
+        (StatusCode::OK, "Busca pessoas");
+    }
+
+    pub async fn create_person(
+        State(people): State<AppState>,
+        Json(new_person): Json<NewPerson>
+    ) -> impl IntoResponse {
+        let id = Uuid::now_v7();
+        let person = Person {
+            id,
+            name: new_person.name,
+            nick: new_person.nick,
+            birth_date: new_person.birth_date,
+            stack: new_person.stack
+        };
+
+        people.write().await.insert(id, person.clone());
+
+        (StatusCode::OK, Json(person))
+    }
+
+    pub async fn count_person(state: State<AppState>) -> impl IntoResponse {
+        let count = state.read().await.len();
+        (StatusCode::OK, Json(count));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod controller;
-use controller::person_controller::{Person, NewPerson};
+use controller::person_controller::{Person, NewPerson, AppState};
 
 use std::{
     collections::HashMap,
@@ -12,7 +12,6 @@ use time::{macros::date, Date};
 use tokio::sync::{Mutex, RwLock};
 use uuid::Uuid;
 
-type AppState = Arc<RwLock<HashMap<Uuid, Person>>>;
 
 #[tokio::main]
 async fn main() {
@@ -34,49 +33,12 @@ async fn main() {
     let app_state: AppState = Arc::new(RwLock::new(people));
 
     let app = Router::new()
-        .route("/pessoas", get(search_people))
-        .route("/pessoas/:id", get(find_person))
-        .route("/pessoas", post(create_person))
-        .route("/contagem-pessoas", get(count_person))
+        .route("/pessoas", get(Person::search_people))
+        .route("/pessoas/:id", get(Person::find_person))
+        .route("/pessoas", post(Person::create_person))
+        .route("/contagem-pessoas", get(Person::count_person))
         .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
-}
-
-async fn search_people(state: State<AppState>) -> impl IntoResponse {
-    (StatusCode::OK, "Busca pessoas");
-}
-
-async fn find_person(
-    State(people): State<AppState>,
-    Path(person_id): Path<Uuid>
-) -> impl IntoResponse {
-    match people.read().await.get(&person_id) {
-        Some(person) => Ok(Json(person.clone())),
-        None => Err(StatusCode::NOT_FOUND)
-    }
-}
-
-async fn create_person(
-    State(people): State<AppState>,
-    Json(new_person): Json<NewPerson>
-) -> impl IntoResponse {
-    let id = Uuid::now_v7();
-    let person = Person {
-        id,
-        name: new_person.name,
-        nick: new_person.nick,
-        birth_date: new_person.birth_date,
-        stack: new_person.stack
-    };
-
-    people.write().await.insert(id, person.clone());
-
-    (StatusCode::OK, Json(person))
-}
-
-async fn count_person(state: State<AppState>) -> impl IntoResponse {
-    let count = state.read().await.len();
-    (StatusCode::OK, Json(count));
 }
